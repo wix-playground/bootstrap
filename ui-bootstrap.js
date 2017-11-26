7cf6c6279fe8caba5182edb3888fd1445720f84c
builduser <builduser@wix.com>
2017-11-26 16:06:48 +0000

    bower version 0.13.6

--- ui-bootstrap.js
+++ ui-bootstrap.js
@@ -2,7 +2,7 @@
  * angular-ui-bootstrap-booking
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 0.13.5 - 2017-11-26
+ * Version: 0.13.6 - 2017-11-26
  * License: MIT
  */
 angular.module("ui.bootstrap", ["ui.bootstrap.collapse","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.bindHtml","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.dateparser","ui.bootstrap.position","ui.bootstrap.datepicker","ui.bootstrap.dropdown","ui.bootstrap.modal","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.transition","ui.bootstrap.typeahead"]);
